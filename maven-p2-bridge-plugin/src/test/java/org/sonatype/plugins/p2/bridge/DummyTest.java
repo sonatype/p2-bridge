bb90a581a295affe8ca7570f78b7fb67e4868917
Jason Dillon <jason@planet57.com>
2013-07-24 14:28:22 -0700

    add dummy test to keep build-automation configuration unified

---
+++ maven-p2-bridge-plugin/src/test/java/org/sonatype/plugins/p2/bridge/DummyTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2007-2013 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.sonatype.plugins.p2.bridge;
+
+import org.junit.Test;
+import org.sonatype.sisu.litmus.testsupport.TestSupport;
+
+/**
+ * FIXME: Replace with actual tests.
+ */
+public class DummyTest
+    extends TestSupport
+{
+    @Test
+    public void fixme() {
+        // Add some real tests dude!
+    }
+}
